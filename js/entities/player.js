526d2995a6836cc4b94b5f19dd26dfcc6dfb2eeb
nick <nick@Butts.littleboy>
2013-06-18 16:48:38 -0500

    changed jump animation names

--- js/entities/player.js
+++ js/entities/player.js
@@ -68,7 +68,7 @@ game.Player = game.Entity.extend({
         this.renderable.addAnimation("stand-right", [0,1,2]);
         this.renderable.addAnimation("right",[3,4,5,6,7,8,9]);
 
-	this.renderable.addAnimation("up", [12,13,14,15,16,17,18,19,20]);
+	this.renderable.addAnimation("jump", [12,13,14,15,16,17,18,19,20]);
  	
 	this.renderable.setCurrentAnimation("stand-right");
     },
