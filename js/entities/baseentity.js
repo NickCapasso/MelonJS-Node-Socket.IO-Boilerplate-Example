a8247d61c1ce7cd50b8f72b80348fbbf9a0ddfb0
Ean Schuesslert <ean@brainfood.com>
2013-06-19 16:04:40 -0500

    Add doAnim utility function. Handle setting the jump animation when we are falling or jumping. Break left/right movement into a separate section than setting the left/right animation.

--- js/entities/baseentity.js
+++ js/entities/baseentity.js
@@ -1,35 +1,47 @@
+var doAnim = function(renderable, animation) {
+    if (!renderable.isCurrentAnimation(animation))
+	renderable.setCurrentAnimation(animation);
+}
+
 game.Entity = me.ObjectEntity.extend({
     init: function (x, y, settings) {
         this.parent(x, y, settings);
     },
 
     update : function () {
-        if (this.moving) {
-
-            // Look into replacing this with dispatch tables
-            // http://designpepper.com/blog/drips/using-dispatch-tables-to-avoid-conditionals-in-javascript
-            switch (this.direction) {
-                case "left":
-                    if (!this.renderable.isCurrentAnimation("left"))
-                        this.renderable.setCurrentAnimation("left");
-                    this.moving = true;
-                    this.vel.x -= this.accel.x * me.timer.tick;                   		    break;
-               
-		case "right":
-                   if (!this.renderable.isCurrentAnimation("right"))
-                        this.renderable.setCurrentAnimation("right");
-                    this.moving = true;
-                    this.vel.x += this.accel.x * me.timer.tick;
-                    break;
-		
-		case "jump":
+	    if (this.jumping || this.falling) {
 		  if (!this.renderable.isCurrentAnimation("jump"))
 		        this.renderable.setCurrentAnimation("jump");
-		    this.moving = true;
-		    this.vel.y += this.accel.y * me.timer.tick;
-		    break;
+		    //this.moving = true;
+		    //this.vel.y += this.accel.y * me.timer.tick;
+            } else {
+		    switch (this.direction) {
+			case "left":
+			    doAnim(this.renderable, 'left');
+			    break;
+		       
+			case "right":
+			   if (!this.renderable.isCurrentAnimation("right"))
+				this.renderable.setCurrentAnimation("right");
+			    break;
+		    }
             }
 
+        if (this.moving) {
+            // Look into replacing this with dispatch tables
+            // http://designpepper.com/blog/drips/using-dispatch-tables-to-avoid-conditionals-in-javascript
+
+		
+	    switch (this.direction) {
+		case "left":
+		    this.moving = false;
+		    this.vel.x -= this.accel.x * me.timer.tick;                   		    break;
+	       
+		case "right":
+		    this.moving = false;
+		    this.vel.x += this.accel.x * me.timer.tick;
+		    break;
+	    }
         }
 
         this.updateMovement();
